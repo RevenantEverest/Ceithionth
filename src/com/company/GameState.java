a5cb1509e9142e1398ab9bf183b2dd3de848d448
Stefan <stefischer@mail.com>
2018-05-21 21:50:49 -0400

    Map generation setup

--- src/com/company/GameState.java
+++ src/com/company/GameState.java
@@ -5,19 +5,23 @@ import java.awt.Graphics;
 public class GameState extends State{
 
     private Player player;
+    private Map map;
 
     public GameState(Game game) {
         super(game);
         player = new Player(game, 100, 100);
+        map = new Map("");
     }
 
     @Override
     public void update() {
+        map.update();
         player.update();
     }
 
     @Override
     public void render(Graphics g) {
+        map.render(g);
         player.render(g);
     }
 }
---
+++ src/com/company/TreeTile.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class TreeTile extends Tile {
+
+    public TreeTile(int id) {
+        super(Assets.tree, id);
+    }
+
+    @Override
+    public boolean isSolid() {
+        return true;
+    }
+}
